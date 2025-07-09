216463e48f601b9c084fbffaa83d399769313454
acleveland <andrew.cleveland@hotmail.co.uk>
2025-07-09 01:25:22 +0100

    AC - set port to 0.0.0.0:8080 to allow external access

--- src/main.rs
+++ src/main.rs
@@ -82,7 +82,7 @@ async fn main() -> std::io::Result<()> {
             .route("/seek_backward", web::post().to(seek_backward))
             .route("/status", web::get().to(status))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
